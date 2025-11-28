ca2265ebf703e5b7add11bd128a6f439878706aa
CobPeruReiner <dalayo@cobranzasperu.com>
2025-11-28 10:52:17 -0500

    correccion rutas de envio y html

--- Backend/app.js
+++ Backend/app.js
@@ -11,20 +11,16 @@ export const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRouter);
+app.use("/api/campania", campainaRouter);
+app.use("/api/cartera", carterasRouter);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/auth", authRouter);
-
-app.use("/api/campania", campainaRouter);
-
-app.use("/api/cartera", carterasRouter);
